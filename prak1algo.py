eb63ffac17c70a497c7d8cd30af56dcc437d9e9c
HAIDAR ZHAFIRAL <125808350+haidarzha@users.noreply.github.com>
2024-09-20 10:58:29 +0700

    Add files via upload

---
+++ prak1algo.py
@@ -0,0 +1,6 @@
+nama = input("Nama kamu siapa? ")
+lokasi = input("Dari mana asal kamu? ")
+sekolah = input("Sebelumnya bersekolah dimana? ")
+print("halo selamat siang", nama)
+print("wah dari", lokasi)
+print("Pasti menyenangkan di", sekolah)
