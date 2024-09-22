88d5af1e7e6fb254c1af64102165f67548e07ff7
HAIDAR ZHAFIRAL <125808350+haidarzha@users.noreply.github.com>
2024-09-22 19:24:40 +0700

    Add files via upload

---
+++ praktikum2.py
@@ -0,0 +1,6 @@
+print("Hitung luas ruangan")
+panjang = int(input("Masukkan panjang ruangan: "))
+lebar = int(input("Masukkan lebar ruangan: "))
+satuan = input("Masukkan Satuan(Meter/Inci) : ")
+luas = panjang * lebar 
+print("Luas ruangan dengan panjang", float(panjang), "dan lebar", float(lebar), "adalah", float(luas), satuan)
